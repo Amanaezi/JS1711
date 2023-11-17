35c6307227831ae86721dfc582edbe1a34e39684
Amanaezi <supercrash2506@gmail.com>
2023-11-17 23:39:31 +0200

    some fixes

--- 3/script.js
+++ 3/script.js
@@ -1,29 +1,29 @@
 let treasure = {
     x: Math.floor(Math.random() * 768),
     y: Math.floor(Math.random() * 570)
-  };
-  
-  let map = document.getElementById("map");
-  let coords = document.getElementById("coords");
-  
-  map.onmousemove = showCoords;
-  map.onmouseover = function () {
+};
+
+let map = document.getElementById("map");
+let coords = document.getElementById("coords");
+
+map.onmousemove = showCoords;
+map.onmouseover = function () {
     coords.value = "Наведіть мишку на карту";
-  };
-  
-  function showCoords(event) {
+};
+
+function showCoords(event) {
     let mouseX = event.offsetX;
     let mouseY = event.offsetY;
     coords.value = "Координаты: " + mouseX + ", " + mouseY;
-  
+
     let distance = getDistance(event);
     if (distance < 30) {
-      alert("Сокровище здесь!");
+        alert("Сокровище здесь!");
     }
-  }
-  
-  function getDistance(event) {
+}
+
+function getDistance(event) {
     let distanceX = event.offsetX - treasure.x;
     let distanceY = event.offsetY - treasure.y;
     return Math.sqrt(distanceX * distanceX + distanceY * distanceY);
-  }
+}
